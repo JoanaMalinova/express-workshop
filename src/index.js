c45d2736556f90f06f467d80fbac4d511730a83d
Joana Malinova <ani.mani.nen@gmail.com>
2023-06-01 14:25:31 +0300

    linked static files to project

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const app = express();
 
 const PORT = 5000;
 
+//Express conig
+app.use(express.static('src/public'));
+
 //Handlebars config
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
@@ -16,6 +19,5 @@ app.set('views', 'src/views');
 app.get('/', (req, res) => {
     res.render('index');
 });
-
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
