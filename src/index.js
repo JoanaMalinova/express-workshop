8c9e471b46e8abcfca46b9e7167d83656b92b9ed
Joana Malinova <ani.mani.nen@gmail.com>
2023-06-01 16:14:32 +0300

    added cubeController

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ const app = express();
 const PORT = 5000;
 
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
 
 //ExpressConfig
 require('./config/expressConfig')(app);
 
 //Handlebars config
-require('./confg/handlebarsConfig')(app);
+require('./config/handlebarsConfig')(app);
 
 app.use(homeController);
+app.use('/cubes', cubeController);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
