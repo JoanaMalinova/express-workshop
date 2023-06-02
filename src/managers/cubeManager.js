4c91a3aec6267979bc24177c161b3fa520e2220e
Joana Malinova <ani.mani.nen@gmail.com>
2023-06-02 13:46:35 +0300

    added search

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,7 +1,24 @@
 const cubes = [];
 const uniqid = require('uniqid');
 
-exports.getAll = () => cubes.slice();
+exports.getAll = (search, from, to) => {
+
+    let result = cubes.slice();
+
+    if (search) {
+        result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    if (from) {
+        result = result.filter(cube => cube.difficultyLevel >= Number(from));
+    }
+
+    if (to) {
+        result = result.filter(cube => cube.difficultyLevel <= Number(to));
+    }
+
+    return result;
+}
 
 exports.getOne = (id) => cubes.find(cube => cube.id === id);
 
