dc8400991610640c359ec26af5d0d22166cd6d95
Joana Malinova <ani.mani.nen@gmail.com>
2023-06-07 13:05:57 +0300

    set up mongoose

--- src/index.js
+++ src/index.js
@@ -7,10 +7,19 @@ const PORT = 5000;
 
 //ExpressConfig
 require('./config/expressConfig')(app);
-
 //Handlebars config
 require('./config/handlebarsConfig')(app);
+
+//mongoose config
+require('./config/mongooseConfig')()
+    .then(() => console.log('DB connected successfuly!'))
+    .catch((err) => console.log(`DB error: ${err.message}`));
 
 app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
+
+
+
+
+
