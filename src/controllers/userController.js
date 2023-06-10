c8ddafedf5047acccdf6528ef86ce0654422a048
Joana Malinova <ani.mani.nen@gmail.com>
2023-06-10 15:40:39 +0300

    created User model

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,21 +1,27 @@
 const router = require('express').Router();
 
 router.get('/login', async (req, res) => {
-    res.render('/user/login');
+    res.render('/users/login');
 });
 
 router.post('/login', async (req, res) => {
 
+    const { username, password } = req.body
+
 })
 
 router.get('/register', async (req, res) => {
-    res.render('/user/register');
+    res.render('/users/register');
 });
 
 router.post('/register', async (req, res) => {
+    const { username, password, repeatPassword } = req.body;
+    res.redirect('/users/login');
 
 })
 
 router.get('/logout', async (req, res) => {
 
 });
+
+module.exports = router;
